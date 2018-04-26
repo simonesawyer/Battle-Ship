065095a260601c26a725b0cbb4494eda3affe8fd
simonesawyer <simonesawyer@users.noreply.github.com>
2018-04-25 21:13:12 -0700

    Delete Destroyer.java

--- Battle-Ship/Destroyer.java
+++
@@ -1,7 +0,0 @@
-public class Destroyer extends AbstractShips {
-
-    public Destroyer(Integer hitCells, Integer size){
-        super(size);
-    }
-    protected  Ships shipFactory(Integer newSize) { return new Destroyer(newSize,0); }
-}
