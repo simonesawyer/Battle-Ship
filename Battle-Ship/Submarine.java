9824155a3cb90c5f256ba06099101f9843049da7
simonesawyer <simonesawyer@users.noreply.github.com>
2018-04-25 21:13:31 -0700

    Delete Submarine.java

--- Battle-Ship/Submarine.java
+++
@@ -1,7 +0,0 @@
-public class Submarine extends AbstractShips {
-
-    public Submarine(Integer hitCells, Integer size){
-        super(size);
-    }
-    protected  Ships shipFactory(Integer newSize) { return new Submarine(newSize,0); }
-}
