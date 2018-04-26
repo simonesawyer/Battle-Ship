540750e34cb2de85856fab7446c4139ca2942ed2
simonesawyer <simonesawyer@users.noreply.github.com>
2018-04-25 21:13:03 -0700

    Delete Cruiser.java

--- Battle-Ship/Cruiser.java
+++
@@ -1,7 +0,0 @@
-public class Cruiser extends AbstractShips {
-
-    public Cruiser(Integer hitCells, Integer size){
-        super(size);
-    }
-    protected  Ships shipFactory(Integer newSize) { return new Cruiser(newSize,0); }
-}
